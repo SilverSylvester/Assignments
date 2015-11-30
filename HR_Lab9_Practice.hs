477dc8402d94406d3005ce252ef7e9afe0b0ed01
SilverSylvester <reynolds.conor@gmail.com>
2015-11-30 13:49:59 +0000

    Nothing important

--- HR_Lab9_Practice.hs
+++ HR_Lab9_Practice.hs
@@ -6,10 +6,10 @@ import Text.Printf
 main = readLn >>= printf "%.3f\n" . birthday
 
 birthday :: Integer -> Double
-birthday n = 1 - (365 `pick` (365 - n + 1)) // (365^n)
+birthday n = 1 - (365 `p` (365 - n + 1)) // (365^n)
 
-pick :: (Integral a) => a -> a -> a
-n `pick` k = product [k..n]
+p :: (Integral a) => a -> a -> a
+n `p` k = product [k..n]
 
 -- Ensures minimal loss of floating point precision
 (//) :: (Fractional a) => Integer -> Integer -> a
