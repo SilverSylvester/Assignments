b171dc5f91e026e972239780e9818838d46a006f
SilverSylvester <reynolds.conor@gmail.com>
2015-12-02 22:04:27 +0000

    Completely revamped the core function to remove redundant dependencies

--- HR_Lab9_Practice.hs
+++ HR_Lab9_Practice.hs
@@ -3,15 +3,11 @@ module HR_Lab9_Practice where
 import Data.Ratio ((%)) 
 import Text.Printf
 
-main = readLn >>= printf "%.3f\n" . birthday
+main = readLn >>= printf "%.3f\n" . recBirthday
 
-birthday :: Integer -> Double
-birthday n = 1 - (365 `p` (365 - n + 1)) // (365^n)
-
-p :: (Integral a) => a -> a -> a
-n `p` k = product [k..n]
-
--- Ensures minimal loss of floating point precision
-(//) :: (Fractional a) => Integer -> Integer -> a
-a // b = fromRational (a % b)
+recBirthday :: Integer -> Double
+recBirthday n = 1 - fromRational (recBirthday' n)
+    where recBirthday' n
+              | n <= 1    = 1
+              | otherwise = ((365 - n + 1) % 365) * recBirthday' (n - 1)
 
