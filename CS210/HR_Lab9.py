49345f1c5d4cef809320363a8873298e6afbd4d5
SilverSylvester <reynolds.conor@gmail.com>
2015-12-07 09:25:01 +0000

    Left out gcd function

--- CS210/HR_Lab9.py
+++ CS210/HR_Lab9.py
@@ -5,6 +5,15 @@ def is_palin(l):
         return is_palin(l[1:-1])
     else: return "FALSE"
 
+def gcd(a, b):
+    if b == 0:
+        return a
+    else: return gcd(b, a % b)
+
 s = input()
 print(is_palin(s))
+
+a = int(input())
+b = int(input())
+print(gcd(a,b))
 
