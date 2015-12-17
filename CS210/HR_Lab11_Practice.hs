ac2a15784b6d83bc3c546a347097681e9c9c999e
SilverSylvester <reynolds.conor@gmail.com>
2015-12-17 15:06:19 +0000

    Function now makes sense

--- CS210/HR_Lab11_Practice.hs
+++ CS210/HR_Lab11_Practice.hs
@@ -8,15 +8,17 @@ type Base = Integer
 main = getContents >>= print . (\(b:b':n:_) -> toBaseN b b' n) . map read . lines
 
 toBaseN :: Base -> Base -> Integer -> Integer
-toBaseN b b' = read . reverse . snd . runWriter . fromBase10 b' . toBase10 b
+toBaseN b b' = fromBase10 b' . toBase10 b
 
 toBase10 :: Base -> Integer -> Integer
 toBase10 b = foldl ((+) . (*b)) 0 . map (fromIntegral . digitToInt) . show
 
-fromBase10 :: Base -> Integer -> Writer String Integer
-fromBase10 b n
-    | n == 0    = return b
-    | otherwise = let (q,r) = n `quotRem` b
-                  in do tell (show r)
-                        fromBase10 b q
+fromBase10 :: Base -> Integer -> Integer
+fromBase10 _ 0 = 0
+fromBase10 b n = read . reverse . snd . runWriter $ fromBase10' b n
+    where fromBase10' b n
+              | n == 0    = return b
+              | otherwise = let (q,r) = n `quotRem` b
+                            in do tell (show r)
+                                  fromBase10' b q
 
